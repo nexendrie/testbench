f07fbf2116fafd7a3e8b18c660209a609f8929b0
Jakub Konečný <jakub.konecny2@centrum.cz>
2024-08-10 21:02:50 +0200

    removed support for run levels

--- src/TCompiledContainer.php
+++ src/TCompiledContainer.php
@@ -21,23 +21,4 @@ trait TCompiledContainer
     {
         return ContainerFactory::create(true, $config);
     }
-
-    protected function changeRunLevel(int $testSpeed = \Testbench::FINE): void
-    {
-        if ((int) getenv('RUNLEVEL') < $testSpeed) {
-            \Tester\Environment::skip(
-                "Required runlevel '$testSpeed' but current runlevel is '" . (int) getenv('RUNLEVEL') . "' (higher runlevel means slower tests)\nYou can run this test with environment variable: 'RUNLEVEL=$testSpeed vendor/bin/run-tests ...'\n"
-            );
-        }
-    }
-
-    protected function markTestAsSlow(bool $really = true): void
-    {
-        $this->changeRunLevel($really ? \Testbench::FINE : \Testbench::QUICK);
-    }
-
-    protected function markTestAsVerySlow(bool $really = true): void
-    {
-        $this->changeRunLevel($really ? \Testbench::SLOW : \Testbench::QUICK);
-    }
 }
